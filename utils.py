34c59858c634cc8759e9ef28b6a0be87e6e1c4cf
Pratul Kumar <pratulkumar2003@gmail.com>
2025-09-07 17:56:50 +0530

    Added Booking classs

--- utils.py
+++ utils.py
@@ -1,18 +1,23 @@
+import datetime as dt
 class Room:
-    def __init__(self, room_number, room_type, price_per_night, is_available):
+    def __init__(self, room_number, room_type, price_per_night):
         self.room_number=room_number
         self.room_type=room_type
         self.price_per_night=price_per_night
-        self.is_available=is_available
+        self.is_available=True
 
     def book_room(self):
-        pass
+        if self.is_available:
+            self.is_available=False
+            return True
+        return False
+    
 
     def checkout_room(self):
-        pass
+        self.is_available=True
 
     def __str__(self):
-        pass
+        return f"Room Number: {self.room_number}, Room Type: {self.room_type}, Price per night: {self.price_per_night}"
 
 class Guest:
     def __init__(self, name, phone_number):
@@ -20,5 +25,17 @@ class Guest:
         self.phone_number=phone_number
 
     def __str__(self):
-        pass
+        return f"Name: {self.name}, Phone Number: {self.phone_number}"
+    
+
+class Booking:
+    def __init__(self, guest, room, check_in_date, check_out_date):
+        self.guest=guest
+        self.room=room
+        self.check_in_date=check_in_date
+        self.check_out_date=check_out_date
+
+    def 
+    
+
 
